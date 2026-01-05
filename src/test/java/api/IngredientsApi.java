f34b2826229c020c33695abf0e60973fa000cafe
Max <angrymaxim777@gmail.com>
2026-01-06 03:20:47 +0400

    edit IngredientsApi.java

--- src/test/java/api/IngredientsApi.java
+++ src/test/java/api/IngredientsApi.java
@@ -1,4 +1,21 @@
 package api;
 
+import io.restassured.response.Response;
+import utils.ApiClient;
+import utils.TestConfig;
+
 public class IngredientsApi {
+
+    public static Response getIngredients() {
+        return ApiClient.get(TestConfig.INGREDIENTS_PATH);
+    }
+
+    public static String[] getValidIngredientIds() {
+        Response response = getIngredients();
+        return response.jsonPath().getString("data._id").split(",");
+    }
+
+    public static String getInvalidIngredientId() {
+        return "invalid_id_12345";
+    }
 }
