cc929bf190960df0784dccb0e02a7388f2ed21dd
Max <angrymaxim777@gmail.com>
2026-01-06 03:19:24 +0400

    edit OrderResponse.java

--- src/test/java/dto/response/OrderResponse.java
+++ src/test/java/dto/response/OrderResponse.java
@@ -1,4 +1,47 @@
 package dto.response;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
 public class OrderResponse {
+
+    private boolean success;
+    private String name;
+    private Order order;
+
+    @JsonIgnoreProperties(ignoreUnknown = true)
+    public static class Order {
+        private int number;
+
+        public int getNumber() {
+            return number;
+        }
+
+        public void setNumber(int number) {
+            this.number = number;
+        }
+    }
+
+    public boolean isSuccess() {
+        return success;
+    }
+
+    public void setSuccess(boolean success) {
+        this.success = success;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Order getOrder() {
+        return order;
+    }
+
+    public void setOrder(Order order) {
+        this.order = order;
+    }
 }
