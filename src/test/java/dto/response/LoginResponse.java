bd2cd2891e381c48f3937f4253b4307b05824dba
Max <angrymaxim777@gmail.com>
2026-01-06 03:20:25 +0400

    edit LoginResponse.java

--- src/test/java/dto/response/LoginResponse.java
+++ src/test/java/dto/response/LoginResponse.java
@@ -1,4 +1,62 @@
 package dto.response;
 
 public class LoginResponse {
+
+    private boolean success;
+    private String accessToken;
+    private String refreshToken;
+    private User user;
+
+    public static class User {
+        private String email;
+        private String name;
+
+        public String getEmail() {
+            return email;
+        }
+
+        public void setEmail(String email) {
+            this.email = email;
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+    }
+
+    public boolean isSuccess() {
+        return success;
+    }
+
+    public void setSuccess(boolean success) {
+        this.success = success;
+    }
+
+    public String getAccessToken() {
+        return accessToken;
+    }
+
+    public void setAccessToken(String accessToken) {
+        this.accessToken = accessToken;
+    }
+
+    public String getRefreshToken() {
+        return refreshToken;
+    }
+
+    public void setRefreshToken(String refreshToken) {
+        this.refreshToken = refreshToken;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
 }
